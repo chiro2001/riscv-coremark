8704b77bb265f3184d97fdf6ed8532a1ddf5413a
Chiro Liang <Chiro2001@163.com>
2022-10-26 21:34:09 +0800

    porting to riscv32

--- build-coremark.sh
+++ build-coremark.sh
@@ -9,8 +9,13 @@ cd $BASEDIR/$CM_FOLDER
 
 # run the compile
 echo "Start compilation"
-make PORT_DIR=../riscv64 compile
-mv coremark.riscv ../
+# make PORT_DIR=../riscv64 compile
+# mv coremark.riscv ../
 
-make PORT_DIR=../riscv64-baremetal compile
+# make PORT_DIR=../riscv64-baremetal compile
+# mv coremark.bare.riscv ../
+
+make PORT_DIR=../riscv32-baremetal compile
+riscv32-unknown-elf-objcopy -O binary coremark.bare.riscv coremark.bare.bin
 mv coremark.bare.riscv ../
+mv coremark.bare.bin ../
