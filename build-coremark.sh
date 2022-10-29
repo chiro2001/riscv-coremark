fe65e2978f8b3560d688958fb9cf7404bde990cf
Chiro Liang <Chiro2001@163.com>
2022-10-29 09:51:49 +0800

    rv32

--- build-coremark.sh
+++ build-coremark.sh
@@ -10,10 +10,14 @@ cd $BASEDIR/$CM_FOLDER
 # run the compile
 echo "Start compilation"
 make PORT_DIR=../riscv64 compile
+riscv64-unknown-elf-objcopy -O binary coremark.riscv coremark.bin
 mv coremark.riscv ../coremark.riscv64
+mv coremark.bin ../coremark.bin64
 
 make PORT_DIR=../riscv64-baremetal compile
+riscv64-unknown-elf-objcopy -O binary coremark.bare.riscv coremark.bare.bin
 mv coremark.bare.riscv ../coremark.bare.riscv64
+mv coremark.bare.bin ../coremark.bare.bin64
 
 make PORT_DIR=../riscv32 compile
 riscv32-unknown-elf-objcopy -O binary coremark.riscv coremark.bin
